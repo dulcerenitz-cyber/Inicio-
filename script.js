82b01b43580263ece7cc8a2c54f763ba3bd4b533
dulcerenitz-cyber <dulcerenitz@gmail.com>
2026-08-02 18:50:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const passwordInput = document.getElementById('passwordInput');
+const toggleIcon = document.getElementById('toggleIcon');
+
+toggleIcon.addEventListener('click', function() {
+    
+    if (passwordInput.type === 'password') {
+    
+        passwordInput.type = 'text';
+        
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash');
+    } 
+    else {
+        
+        passwordInput.type = 'password';
+        
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye');
+    }
+});
